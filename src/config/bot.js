8d40a0abe3e074b702a69ae88f38410ffd90f85b
w-174 <w-174@bk.ru>
2026-03-16 10:13:02 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#000080", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
